ce5fa7d2c68dd065fa8c4e1c7da0d3380d078505
hribeiro <hugo@moodlar.pt>
2024-12-12 12:22:07 +0000

    Use shortname and username to batch enrol

--- batchEnrol.py
+++ batchEnrol.py
@@ -5,7 +5,7 @@ file = sys.argv[1]
 df = pd.read_csv(file)
 moodle_path = '/var/www/html/moodledev/'
 
-data = df.groupby('course')['useridnumber'].apply(list).to_dict()
+data = df.groupby('courseshortname')['username'].apply(list).to_dict()
 
 # Initialize a list to store the resulting strings
 result_strings = []
@@ -15,12 +15,10 @@ for course, user_ids in data.items():
     result_strings.append(result)
 
 for result in result_strings:
-    command = 'moosh -n course-enrol ' + result
-    # print (command)
+    command = 'moosh -n course-enrol -s ' + result
     try:
         call = subprocess.run(command, shell=True, check=True, capture_output=True, text=True, cwd=moodle_path)
         print(f"Command: {command}")
-       # print(f"Backup Path:\n{result.stdout}")
     except subprocess.CalledProcessError as e:
         print(f"Failed course: {course}.")
         print(f"Error Output:\n{e.stderr}")
